03bf6e4f382aa19e676352b6e088da9ccb3026e1
Alvin Pratama <113691381+palveeen22@users.noreply.github.com>
2024-10-19 17:03:31 +0700

    add: added mock data for reder

---
+++ src/screens/HomeScreen/constants/index.ts
@@ -0,0 +1,27 @@
+import { TMenuItem as MenuItemType } from '../types';
+
+export const filterMenu: (string | { uri: string })[] = [
+    { uri: 'https://utfs.io/f/EbyVGwMXeijrE2NsYAXeijrKsCGtR1xBUM5JP4DuQXlbom0L' },
+    // '↑↓',
+    'Новинки',
+    'Лучшее',
+    'Суперкэшбек',
+    'Горячие блюда',
+];
+
+// export const filterMenu: string[] = [
+//     // { uri: 'https://utfs.io/f/EbyVGwMXeijrE2NsYAXeijrKsCGtR1xBUM5JP4DuQXlbom0L' },
+//     'Новинки',
+//     'Лучшее',
+//     'Суперкэшбек',
+//     'Горячие блюда',
+// ];
+
+export const menuItems: MenuItemType[] = [
+    { id: 1, name: 'Яйца пашот на бриоши с пастрами', disc: 2, weight: '350 гр.', price: 540, image: 'https://s3-alpha-sig.figma.com/img/ebe9/6177/5935ef840e4ae77d7511a27eace204c9?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=fJPO7zAHUQYiQuXTNhgRMUhQGSBf8K7yB7pXE0xxRGnJmYPdN7GnExl3iQe51fRwh6IEd7TS1rr44GBsU-DVw-Owhq9UBX9~sRIR~nGfBiqdcPusrdMRewyqdVPwrj6rhlsGTRN511RaJsbs5-eNu0PtT-YOsLyhO5LocBf49-5LSHJh0khOn-tC~MtHs4Q-pOG8gNw6ZeY6GZJKUnCzUU2Lcfaom5I86fo~LfRyPBXOUMpdc1PqzTbAO4a49I8ZES71bcWYa6b4Ms9PUJNPTFlyioH1foapqyekDTxTfijK6Fpb5ldsUqUYWBI05K-ypWNikjohqG55gXYojHScmA__' },
+    { id: 2, name: 'Breakfast burger', disc: 2, weight: '350 гр.', price: 540, image: 'https://s3-alpha-sig.figma.com/img/cce0/ed93/946a9fddadb01a14064bc998e23d3133?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=pwovnfz-o7kPRwlIOHPqDpfDWVLjCaLBKDqVdz6E9UGqe99rHWP59qV9Nsbo~YfTmRlfgf7q~Rk5YXZxTyNmYNz5QmMJHYquH6H3b1TXmSx-IuymU8NXnX1wqgQx-DA-Kioxg5nOWdnc~PeS9TFECFi2YoZVP1IMqI4~prYWGRe3CEo5gXGMOl-3EU212einWeD2KAMh4cRMK0ZBikb3D8zNYmseci56mYtcw~7gNQYvdW-UPSg~~x0qcRgXBTk7vNG0Du4wNnlmHzd5N6sPs71LcZWbf5f7VFd3hRAeFJnTHKD00ni~mOQQJHr2ZD5M-KMJtsJBkmYThBc4me-HcA__' },
+    { id: 3, name: 'Яйца пашот на бриоши с пастрами', disc: 2, weight: '350 гр.', price: 540, image: 'https://s3-alpha-sig.figma.com/img/ebe9/6177/5935ef840e4ae77d7511a27eace204c9?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=fJPO7zAHUQYiQuXTNhgRMUhQGSBf8K7yB7pXE0xxRGnJmYPdN7GnExl3iQe51fRwh6IEd7TS1rr44GBsU-DVw-Owhq9UBX9~sRIR~nGfBiqdcPusrdMRewyqdVPwrj6rhlsGTRN511RaJsbs5-eNu0PtT-YOsLyhO5LocBf49-5LSHJh0khOn-tC~MtHs4Q-pOG8gNw6ZeY6GZJKUnCzUU2Lcfaom5I86fo~LfRyPBXOUMpdc1PqzTbAO4a49I8ZES71bcWYa6b4Ms9PUJNPTFlyioH1foapqyekDTxTfijK6Fpb5ldsUqUYWBI05K-ypWNikjohqG55gXYojHScmA__' },
+    { id: 4, name: 'Breakfast burger', disc: 2, weight: '350 гр.', price: 540, image: 'https://s3-alpha-sig.figma.com/img/cce0/ed93/946a9fddadb01a14064bc998e23d3133?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=pwovnfz-o7kPRwlIOHPqDpfDWVLjCaLBKDqVdz6E9UGqe99rHWP59qV9Nsbo~YfTmRlfgf7q~Rk5YXZxTyNmYNz5QmMJHYquH6H3b1TXmSx-IuymU8NXnX1wqgQx-DA-Kioxg5nOWdnc~PeS9TFECFi2YoZVP1IMqI4~prYWGRe3CEo5gXGMOl-3EU212einWeD2KAMh4cRMK0ZBikb3D8zNYmseci56mYtcw~7gNQYvdW-UPSg~~x0qcRgXBTk7vNG0Du4wNnlmHzd5N6sPs71LcZWbf5f7VFd3hRAeFJnTHKD00ni~mOQQJHr2ZD5M-KMJtsJBkmYThBc4me-HcA__' },
+    { id: 5, name: 'Яйца пашот на бриоши с пастрами', disc: 2, weight: '350 гр.', price: 540, image: 'https://s3-alpha-sig.figma.com/img/ebe9/6177/5935ef840e4ae77d7511a27eace204c9?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=fJPO7zAHUQYiQuXTNhgRMUhQGSBf8K7yB7pXE0xxRGnJmYPdN7GnExl3iQe51fRwh6IEd7TS1rr44GBsU-DVw-Owhq9UBX9~sRIR~nGfBiqdcPusrdMRewyqdVPwrj6rhlsGTRN511RaJsbs5-eNu0PtT-YOsLyhO5LocBf49-5LSHJh0khOn-tC~MtHs4Q-pOG8gNw6ZeY6GZJKUnCzUU2Lcfaom5I86fo~LfRyPBXOUMpdc1PqzTbAO4a49I8ZES71bcWYa6b4Ms9PUJNPTFlyioH1foapqyekDTxTfijK6Fpb5ldsUqUYWBI05K-ypWNikjohqG55gXYojHScmA__' },
+    { id: 6, name: 'Breakfast burger', disc: 2, weight: '350 гр.', price: 540, image: 'https://s3-alpha-sig.figma.com/img/cce0/ed93/946a9fddadb01a14064bc998e23d3133?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=pwovnfz-o7kPRwlIOHPqDpfDWVLjCaLBKDqVdz6E9UGqe99rHWP59qV9Nsbo~YfTmRlfgf7q~Rk5YXZxTyNmYNz5QmMJHYquH6H3b1TXmSx-IuymU8NXnX1wqgQx-DA-Kioxg5nOWdnc~PeS9TFECFi2YoZVP1IMqI4~prYWGRe3CEo5gXGMOl-3EU212einWeD2KAMh4cRMK0ZBikb3D8zNYmseci56mYtcw~7gNQYvdW-UPSg~~x0qcRgXBTk7vNG0Du4wNnlmHzd5N6sPs71LcZWbf5f7VFd3hRAeFJnTHKD00ni~mOQQJHr2ZD5M-KMJtsJBkmYThBc4me-HcA__' },
+];
