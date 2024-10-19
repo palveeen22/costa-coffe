e9c216641da47afdde1a935dcf5f38fe715ccaa2
Alvin Pratama <113691381+palveeen22@users.noreply.github.com>
2024-10-19 17:03:20 +0700

    add: added types

---
+++ src/screens/HomeScreen/types/index.ts
@@ -0,0 +1,12 @@
+export type TMenuItem = {
+    id: number;
+    name: string;
+    disc: number
+    weight: string;
+    price: number;
+    image: string;
+  }
+  export type TOrderItem = {
+    [key: number]: number;
+  }
+  export type TCategoryType = string | { uri: string };
